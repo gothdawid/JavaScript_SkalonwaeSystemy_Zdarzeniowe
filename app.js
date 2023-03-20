16fe8886e7ae633a94201d69848ab5203178153f
gothdawid <dawidstu@gmail.com>
2023-03-20 01:01:54 +0100

    delete

--- app.js
+++ app.js
@@ -8,14 +8,31 @@ var urlencoder = bodyParser.urlencoded({ extended: false });
 app.set("view engine", "ejs");
 
 //var msg = "Backend Message!!!";
-var msg = ["Pusta lista"];
+var BooksList = [];
+var error;
 
 app.get("/", function (req, res) {
-  res.render("index", { titles: msg });
+  res.render("index", { titles: BooksList, error: error });
+  error = "";
 });
 
 app.post("/titles", urlencoder, function (req, res) {
-  msg.push(req.body.title);
+  console.log(req.body);
+  if (req.body.method == "delete") {
+    BooksList.splice(req.body.id, 1);
+  } else if (
+    req.body.title == "" ||
+    req.body.title == null ||
+    req.body.author == "" ||
+    req.body.author == null
+  ) {
+    error = "Pole nie może być puste";
+    res.redirect("/");
+    return;
+  } else {
+    const book = { title: req.body.title, author: req.body.author };
+    BooksList.push(book);
+  }
   res.redirect("/");
 });
 
