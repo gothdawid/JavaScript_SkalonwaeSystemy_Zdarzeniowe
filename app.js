80034236ff1919fa0ceeb39ad8edf88a9d5b7f11
gothdawid <dawidstu@gmail.com>
2023-03-20 01:43:17 +0100

    delete & insert to DB

--- app.js
+++ app.js
@@ -5,21 +5,48 @@ const app = express();
 const bodyParser = require("body-parser");
 var urlencoder = bodyParser.urlencoded({ extended: false });
 
+var mongoose = require("mongoose");
+var mongoDB = "mongodb://127.0.0.1/books";
+
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error"));
+
+//Define schema
+var Schema = mongoose.Schema;
+var PersonModelSchema = new Schema({
+  firstName: String,
+  lastName: String,
+  age: Number,
+});
+
+var BookModelSchema = new Schema({
+  title: String,
+});
+
+// Compile model from schema
+var PersonModel = mongoose.model("PersonModel", PersonModelSchema);
+var BookModel = mongoose.model("BookModel", BookModelSchema);
+
 app.set("view engine", "ejs");
 
 //var msg = "Backend Message!!!";
 var BooksList = [];
 var error;
 
-app.get("/", function (req, res) {
+app.get("/", async function (req, res) {
+  BooksList = await BookModel.find();
+  //console.log(BooksList);
   res.render("index", { titles: BooksList, error: error });
   error = "";
 });
 
-app.post("/titles", urlencoder, function (req, res) {
+app.post("/titles", urlencoder, async function (req, res) {
   console.log(req.body);
   if (req.body.method == "delete") {
-    BooksList.splice(req.body.id, 1);
+    //BooksList.splice(req.body.id, 1);
+    var ret = await BookModel.findOneAndRemove({ _id: req.body.id });
+    console.log(ret);
   } else if (
     req.body.title == "" ||
     req.body.title == null ||
@@ -30,8 +57,10 @@ app.post("/titles", urlencoder, function (req, res) {
     res.redirect("/");
     return;
   } else {
-    const book = { title: req.body.title, author: req.body.author };
-    BooksList.push(book);
+    // var person = new PersonModel({ firstName: "John", lastName: "Doe", age: 26 });
+    var book = new BookModel({ title: req.body.title });
+
+    book.save();
   }
   res.redirect("/");
 });
---
+++ start_db.bat
@@ -0,0 +1,2 @@
+.\mongodb-win32-x86_64-windows-6.0.5\bin\mongod.exe --dbpath=".\db"
+pause
